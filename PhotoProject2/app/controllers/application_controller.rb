e2c9e215965abb78b039541984af4d875ed71a0d
Skillvendor <Skillvendor193@gmail.com>
2015-07-30 12:16:40 +0300

    Merge Interfata

--- PhotoProject2/app/controllers/application_controller.rb
+++ PhotoProject2/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
+  before_filter :render_single_page
+
+
+   def render_single_page
+    render 'layouts/application' if request.format == Mime::HTML
+  end
 end
